bf6cf7fcfe07dd2dbeb3a42adac5dfe6ae276dfe
rob <robertmaticke12@gmail.com>
2022-08-12 21:10:58 -0400

    commit tree 532198e6a7ed1c0948fbf27fd271a192b21f0d0a

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(location){
+    if (location >= 42){
+        return location - 42
+    } else if (location <= 41){
+        return 42 - location
+    }
+}
+
+function distanceFromHqInFeet(feet){
+    return distanceFromHqInBlocks(feet) * 264
+}
+
+function distanceTravelledInFeet(start, destination){
+    let southDistance = (destination-start)*264
+    let northDistance = (start-destination)*264
+    if (start > destination){
+        return northDistance
+    } else if (destination > start)
+        return southDistance
+}
+
+function calculatesFarePrice(start, destination){
+    let farePrice
+    let southDistance = (destination-start)*264
+    let northDistance = (start-destination)*264
+    if (start > destination){
+        if (northDistance > 2500){
+            return 'cannot travel that far'
+        } else if (northDistance > 2000){
+            farePrice = 25
+            return farePrice
+        } else if (northDistance > 400){
+            farePrice = (northDistance-400) * 0.02
+            return farePrice
+        } else if (northDistance <= 400){
+            farePrice = 0
+            return farePrice
+        }
+    } else if (destination > start){
+        if (southDistance > 2500){
+            return 'cannot travel that far'
+        } else if (southDistance > 2000){
+            farePrice = 25
+            return farePrice
+        } else if (southDistance > 400){
+            farePrice = (southDistance-400) * 0.02
+            return farePrice
+        } else if (southDistance <= 400){
+            farePrice = 0
+            return farePrice
+        }
+    }
+}
